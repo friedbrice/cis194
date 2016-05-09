91a9305123acd1200bfd5df48d1d8e6e011efe9a
Daniel Brice <danielbrice@gmail.com>
2016-05-09 12:00:50 -0700

    still working

--- week02/LogAnalysis.hs
+++ week02/LogAnalysis.hs
@@ -13,11 +13,20 @@ import Log
 
 -- | @parseMessage s@ parses string @s@ as a @LogMessage@.
 parseMessage :: String -> LogMessage
-parseMessage s@(c : rest) = LogMessage kind timestamp message
+parseMessage s@(c : ' ' : rest) = case c of
+  'E' -> LogMessage (Error lvl) ts msg
+    where
+      lvl = read . head . words $ rest
+      ts = read . head . tail . words $ rest
+      msg = unwords . tail . tail . words $ rest
+  'I' -> LogMessage Info ts1 msg1
+  'W' -> LogMessage Warning ts1 msg1
+  _ -> Unknown s
   where
-    kind = case c of
-      'I' -> Info
-      'W' -> Warning
-      'E' -> Error lvl
-    lvl =
+    ts1 = read . head . words $ rest
+    msg1 = unwords . tail . words $ rest
+parseMessage s = Unknown s
 
+-- | @parse s@ parses the string @s@ as a list of *LogMessage@s.
+parse :: String -> [LogMessage]
+parse s = map parseMessage . lines $ s
