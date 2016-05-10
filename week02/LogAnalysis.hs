a2133cb3e34df4f5751d3f1f29093945875ece07
Daniel Brice <danielbrice@gmail.com>
2016-05-10 14:04:54 -0700

    done with week 2

--- week02/LogAnalysis.hs
+++ week02/LogAnalysis.hs
@@ -27,6 +27,53 @@ parseMessage s@(c : ' ' : rest) = case c of
     msg1 = unwords . tail . words $ rest
 parseMessage s = Unknown s
 
--- | @parse s@ parses the string @s@ as a list of *LogMessage@s.
+-- | @parse s@ parses the string @s@ as a list of @LogMessage@s.
 parse :: String -> [LogMessage]
 parse s = map parseMessage . lines $ s
+
+----------------------
+-- ## Exercise 2 ## --
+----------------------
+
+-- | @insert lmsg msgt@ inserts @lmsg :: LogMessage@ to @msgt :: MessageTree@.
+insert :: LogMessage -> MessageTree -> MessageTree
+insert _ (Node _ (Unknown _) _) = Leaf -- we should never hit this case
+insert (Unknown _) msgt = msgt
+insert lmsg Leaf = Node Leaf lmsg Leaf
+insert lmsg@(LogMessage _ ts1 _) (Node left center@(LogMessage _ ts2 _) right)
+  = if ts1 < ts2
+    then Node (insert lmsg left) center right
+    else Node left center (insert lmsg right)
+
+----------------------
+-- ## Exercise 3 ## --
+----------------------
+
+-- | @build@ builds a @MessageTree@ from a @[LogMessage]@.
+build :: [LogMessage] -> MessageTree
+build [] = Leaf
+build (x : xs) = insert x (build xs)
+
+----------------------
+-- ## Exercise 4 ## --
+----------------------
+
+-- | @inOrder@ creates a @[LogMessage]@ ordered by timestamp.
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node Leaf c r) = c : inOrder r
+inOrder (Node l c r) = inOrder l ++ inOrder (Node Leaf c r)
+
+----------------------
+-- ## Exercise 5 ## --
+----------------------
+
+-- | @whatWentWrong lmsgs@ takes a (potentially) unsorted @[LogMessage]@
+--   and returns the @Error@s (in order) with severity at least 50.
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong lmsgs = map justMsg . filter hiSev . inOrder . build $ lmsgs
+  where
+    hiSev (LogMessage (Error lvl) _ _) = lvl >= 50
+    hiSev _ = False
+    justMsg (LogMessage _ _ msg) = msg
+    justMsg _ = ""
